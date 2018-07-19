ace7208b511a9d88924a3fa100f0e224192062df
jakesamuellawrence <jakesamuellawrence@gmail.com>
2018-07-19 15:23:59 +0100

    Basic spectrum endpoint functionality
    - Addded /spectrum/{octile} view function which returns data from a csv file
    - Removed /hello endpoint function

--- spectral-service/__init__.py
+++ spectral-service/__init__.py
@@ -3,10 +3,19 @@ import flask
 
 def create_app(test_config=None): # Application Factory. Creates an instance of the Flask class and sets things up
 	app = flask.Flask(__name__) # __name__ is the name of the current Python module
-	
-	# A hello world test
-	@app.route("/hello")
-	def hello():
-		return("Hello, World!")
-		
+
+	# Spectrum endpoint. Takes octile as an integer between 1 and 7 returns the data from the respectively numbered file in the form of List<List<double>>
+	@app.route("/spectrum/<octile>")
+	def test(octile):
+		file = open("spectral-data/SKY.SPE000" + octile + ".trim", "r")
+		#return("spectral-data/SKY.SPE000" + octile + ".trim" + file.read())
+		to_return = [] # to_return is array of arrays, to be filled and then returned
+		lines = file.readlines() # lines is array of strings, each string being a line in the file
+		for line in lines:
+			words = line.split() # Separates line into smaller strings with space as the delimiter
+			for i in range(len(words)):
+				words[i] = float(words[i])
+			to_return.append(words)
+		return(flask.jsonify(to_return))
+
 	return(app)

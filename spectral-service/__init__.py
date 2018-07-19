c4d123cd1d5ea21fd2ec64dc1e2f1d44a9bbc635
jakesamuellawrence <jakesamuellawrence@gmail.com>
2018-07-19 16:19:38 +0100

    Initial Commit
    Added splatalogue function definition and routing

--- spectral-service/__init__.py
+++ spectral-service/__init__.py
@@ -3,10 +3,9 @@ import flask
 
 def create_app(test_config=None): # Application Factory. Creates an instance of the Flask class and sets things up
 	app = flask.Flask(__name__) # __name__ is the name of the current Python module
-	
-	# A hello world test
-	@app.route("/hello")
-	def hello():
-		return("Hello, World!")
+		
+	@app.route("/splatalogue")
+	def splatalogue():
+		
 		
 	return(app)
